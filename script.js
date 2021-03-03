7d91e2d460bc6f93c8aff832bb7f0082255738b9
Rishi Ahuja <69950416+Rishi-Ahuja@users.noreply.github.com>
2021-03-03 16:20:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-var x="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var y="abcdefghijklmnopqrstuvwxyz";
-var z="0123456789";
-var a="!@#$%^&*_-";
+var upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower="abcdefghijklmnopqrstuvwxyz";
+var integers="0123456789";
+var special="!@#$%^&*_-";
 var ps="";
-var b=document.getElementById('UP');
-var c=document.getElementById('lp');
-var d=document.getElementById('num');
-var e=document.getElementById('sc');
-if (b.checked=true){
-  var r=Math.floor(Math.random()*25);
-  ps=ps+x[r];
-}
-if (c.checked=true){
-  var r=Math.floor(Math.random()*25);
-  ps=ps+y[r];
-}
-if (d.checked=true){
-  var r=Math.floor(Math.random()*9);
-  ps=ps+z[r];
-}
-if (e.checked=true){
-  var r=Math.floor(Math.random()*9);
-  ps=ps+a[r];
-}
+var a=document.getElementById('UP');
+var b=document.getElementById('lp');
+var c=document.getElementById('num');
+var d=document.getElementById('sc');
 function generate(){
-  document.getElementById('rt').innerHTML(ps); 
+  ps=document.getElementById('keyword').value
+  var g=ps.length;
+  if (a.checked){
+  ps=ps+upper[Math.floor(Math.random()*25)];
+  }
+  if (b.checked){
+  ps=ps+lower[Math.floor(Math.random()*25)];
+  }
+  if (c.checked){
+  ps=ps+integers[Math.floor(Math.random()*9)];
+  }
+  if (d.checked){
+  ps=ps+special[Math.floor(Math.random()*9)];
+  }
+  document.getElementById('rt').innerHTML=ps;
 }
